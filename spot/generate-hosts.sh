1e1167b7cf8eb1c68f163bc71bea84359a9e0e57
SriNivas <sreenivasyandamuri44@gmail.com>
2020-10-07 23:41:33 +0530

    ansible roboshop

--- spot/generate-hosts.sh
+++ spot/generate-hosts.sh
@@ -6,6 +6,6 @@ for i in *.json ; do
   COMPONENT=$(echo $i | sed -e 's/.json//')
   IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
   echo $IP component=$COMPONENT >>/tmp/hosts
-#  sed -e "s/IPADDRESS/${IP}/" -e "s/DNS_NAME/${COMPONENT}/" record.json >/tmp/record.json
-#  aws route53 change-resource-record.json-sets --hosted-zone-id Z000052511SHFQ7DL9WLG --change-batch file:///tmp/record.json.json
+  sed -e "s/IPADDRESS/${IP}/" -e "s/DNS_NAME/${COMPONENT}/" record.json >/tmp/record.json
+  aws route53 change-resource-record.json-sets --hosted-zone-id Z000052511SHFQ7DL9WLG --change-batch file:///tmp/record.json.json
 done
